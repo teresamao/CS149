4f1db66d64505b597cb626e344c6335577386edb
alexgilham <alexander.gilham@gmail.com>
2014-02-23 23:37:17 -0800

    Alexs Code

---
+++ CS 149-2 Assignment #2, TeamIlluminati/Process.java
@@ -0,0 +1,29 @@
+package cs149hw2;
+
+import java.util.Random;
+
+public class Process {
+	
+	public float arrivalTime;
+	public float runTime;
+	public int priority;
+	public char name;
+	
+	public Process()
+	{
+		Random rnd = new Random();
+		arrivalTime = Math.round(rnd.nextFloat() * 99);
+		runTime = Math.round(rnd.nextFloat() * 100);
+		runTime = runTime/10;
+		priority = rnd.nextInt(4) + 1;
+	}
+
+	public void setName(char newName){
+		name = newName;
+	}
+	
+	
+//	public void setSeed(int i){
+//		seed = seed + i;
+//	}
+}
---
+++ CS 149-2 Assignment #2, TeamIlluminati/ProcessScheduler.java
@@ -0,0 +1,57 @@
+package cs149hw2;
+
+import java.util.ArrayList;
+
+public class ProcessScheduler {
+	public static void main (String[] args){
+		
+		char name = 'a';
+		Process[] processes = new Process[10];
+		for(int i = 0; i < 10; i++){
+			processes[i] = new Process();
+			processes[i].setName(name);
+			name++;
+		}
+		
+		processes = sortByArrival(processes);
+		
+//		ArrayList<Process> sortedProcess = new ArrayList<Process>();
+//		for(int i = 0; i < 10; i++)
+//		{
+//			sortedProcess.add(processes[i]);
+//		}
+		
+//		int waitingTime = 0;
+//		int time = 0;
+//		
+//		for(int i = 0; i < 10; i++)
+//		{
+//			waitingTime += waitingTime + processes[i].runTime;
+//			time += processes[i].runTime;
+//		}
+			
+		for(int i = 0; i < 10; i++){
+		System.out.println("Process Name: " + processes[i].name);
+		System.out.println("Arrival Time: " + processes[i].arrivalTime);
+		System.out.println("Run Time: " + processes[i].runTime);
+		System.out.println("Priority: " + processes[i].priority);
+		}
+	}
+	
+	public static Process[] sortByArrival(Process[] p){
+		for(int i = 1; i < p.length; i++){
+			Process temp = p[i];
+			int j;
+			for(j = i - 1; j >= 0 && temp.arrivalTime < p[j].arrivalTime; j--)
+				p[j+1] = p[j];
+			p[j+1] = temp;
+		}
+		return p;
+	}
+	
+	public static void timeChart(ArrayList<Process> p)
+	{
+		
+	}
+	
+}
