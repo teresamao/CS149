f75ac628a9b36753c40c813906184e5596d61e0c
teresamao <teresamao222@gmail.com>
2014-02-24 17:29:22 -0800

    merge changes; conflicts fixed

--- CS 149-2 Assignment #2, TeamIlluminati/ProcessScheduler.java
+++ CS 149-2 Assignment #2, TeamIlluminati/ProcessScheduler.java
@@ -8,30 +8,13 @@ public class ProcessScheduler {
 
         ArrayList<Process> plist = ProcessManager.generateProcesses(50);
         ProcessManager.printProcessList(plist);
-<<<<<<< HEAD
+//        nonpreemptive(plist, "FCFS");
+//        nonpreemptive(plist, "SJF");
+//        nonpreemptive(plist, "HPF");
+
         nonpreemptive(plist, "FCFS");
-        nonpreemptive(plist, "SJF");
-        nonpreemptive(plist, "HPF");
-=======
-        System.out.println(" ");
-        Collections.sort(plist, ProcessComparators.arrivalTimeComparator);
-        ProcessManager.printProcessList(plist);
-
-
-        FCFS(plist);
         fcfsTimeChart(plist);
 
-	}
-	
-	public static Process[] sortByArrival(Process[] p){
-		for(int i = 1; i < p.length; i++){
-			Process temp = p[i];
-			int j;
-			for(j = i - 1; j >= 0 && temp.getArrivalTime() < p[j].getArrivalTime(); j--)
-				p[j+1] = p[j];
-			p[j+1] = temp;
-		}
-		return p;
 	}
 	
 	public static void fcfsTimeChart(ArrayList<Process> p)
@@ -55,7 +38,6 @@ public class ProcessScheduler {
 		}
 		for(int i = 0; i < timeChart.length; i++)
 			System.out.print(timeChart[i]);
->>>>>>> c2bc2ce5891b3941070abf95b231e66f50561f05
 	}
 
     public static void nonpreemptive(ArrayList<Process> list, String type) {
@@ -97,43 +79,5 @@ public class ProcessScheduler {
         System.out.println("Average waiting    = " + (totalWaitTime / throughput));
         System.out.println("Average response   = " + (totalResponseTime / throughput));
     }
-<<<<<<< HEAD
-
-}
-=======
-	
 }
 
-//old code
-//char name = 'a';
-//Process[] processes = new Process[10];
-//for(int i = 0; i < 10; i++){
-//	processes[i] = new Process();
-//	processes[i].setName(name);
-//	name++;
-//}
-//
-//processes = sortByArrival(processes);
-
-//ArrayList<Process> sortedProcess = new ArrayList<Process>();
-//for(int i = 0; i < 10; i++)
-//{
-//	sortedProcess.add(processes[i]);
-//}
-
-//int waitingTime = 0;
-//int time = 0;
-//
-//for(int i = 0; i < 10; i++)
-//{
-//	waitingTime += waitingTime + processes[i].runTime;
-//	time += processes[i].runTime;
-//}
-//
-//for(int i = 0; i < 10; i++){
-//System.out.println("Process Name: " + processes[i].name);
-//System.out.println("Arrival Time: " + processes[i].arrivalTime);
-//System.out.println("Run Time: " + processes[i].runTime);
-//System.out.println("Priority: " + processes[i].priority);
-//}
->>>>>>> c2bc2ce5891b3941070abf95b231e66f50561f05
